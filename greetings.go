ce475076dd0ba85b1fa720cce09ac96b1588e718
darionic96 <dario.nicolosi.g@kdmforce.com>
2026-01-06 18:22:19 +0100

    renamed function name

--- greetings.go
+++ greetings.go
@@ -3,7 +3,7 @@ package greetings
 import "fmt"
 
 // hello returns a greeting for the named person.
-func hello(name string) string {
+func Hello(name string) string {
 	var msg = fmt.Sprintf("Hello %v!", name)
 	return msg
 }
